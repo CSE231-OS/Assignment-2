f0ccc796aa238f18187ad1f82b63e8f9aac20848
Ankur Bohra <ankurbohra04@gmail.com>
2023-09-09 15:28:27 +0530

    Add piping

--- fib.c
+++ fib.c
@@ -6,8 +6,12 @@ int fib(int n){
     return fib(n - 1) + fib(n - 2);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    int n;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s n\n", argv[0]);
+        exit(0);
+    }
+    int n = atoi(argv[1]);
     printf("%d\n", fib(n));
 }
--- main.c
+++ main.c
@@ -5,59 +5,114 @@
 #include <stdlib.h>
 #include <string.h>
 
-int create_process_and_run(char **command){
+int create_process_and_run(char **command, int fds[2]){
     int shell_status = fork();
     if (shell_status < 0){
-        perror("Failed Fork\n");
+        perror("Failed fork");
         exit(0);
     } else if (shell_status == 0){
+        if (fds != NULL) {
+            close(fds[0]);
+            dup2(fds[1], STDOUT_FILENO);
+        }
         execvp(command[0], command);
-        perror("Failed Execution\n");
+        perror("Failed execution");
         exit(0);
     } else {
         int ret;
         int pid = wait(&ret);
         if (!WIFEXITED(ret)){
-            printf("Abnormal Termination of %d\n", pid);
+            printf("Abnormal termination of %d\n", pid);
         }
     }
     return shell_status;
 }
 
-int launch(char **command){
-    int shell_status;
-    shell_status = create_process_and_run(command);
-    return shell_status;
+int launch(char **command, int n, int *offsets){
+    // Shell's input stream should not be affected by piping
+    if (fork() == 0) {
+        if (n == 1) {
+            // No need to redirect stdout
+            return create_process_and_run(command + offsets[0], NULL);
+        } else {
+            int fds[2];
+            pipe(fds);
+
+            create_process_and_run(command + offsets[0], fds);
+
+            close(fds[1]);
+            dup2(fds[0], STDIN_FILENO);
+            int status;
+            wait(&status);
+            if (status != 0) {
+                fprintf(stderr, "Stopping pipe\n");
+                exit(0);
+            }
+            launch(command, n-1, offsets+1);
+        }
+        exit(0);
+    } else {
+        wait(NULL);
+    }
 }
 
-char **read_user_input(char *input, char **command){
-    char *delim = " ";
-    char *word;
-    int i = 0;
-    word = strtok(input, delim);
-    while (word != NULL){
-        command[i] = word;
-        ++i;
-        word = strtok(NULL, delim);
+int read_user_input(char *input, char **command, int *n, int *offsets){
+    char *token, *subtoken;
+    *n = 1;
+    int k = 0;  // Corresponds to each word
+    int pipeOpen = 0;
+    offsets[0] = 0;
+    for (int i = 0; (token = strsep(&input, " ")) && pipeOpen != -1; i++) {
+        for (int j = 0; (subtoken = strsep(&token, "|")); j++) {
+            if (j >= 1) {
+                if (pipeOpen == 1) {
+                    fprintf(stderr, "Invalid command: empty pipe\n");
+                    pipeOpen = -1;
+                    break;
+                }
+                command[k++] = NULL;
+                offsets[*n] = k;
+                *n += 1;
+                pipeOpen = 1;
+            }
+            if (strlen(subtoken) > 0) {
+                pipeOpen = 0;
+                command[k++] = subtoken;
+            }
+        }
     }
-    command[i] = NULL;
-    return command;
+    if (pipeOpen == 1) {
+        fprintf(stderr, "Invalid command: open pipe\n");
+        pipeOpen = -1;
+    }
+    command[k] = NULL;
+    if (pipeOpen == -1) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
 void shell_loop()
 {
     int shell_status;
-    char *input;
-    char **command;
+    char *input = malloc(sizeof(char)*256);
+    char **command = malloc(sizeof(char *)*128);
+    char *cwd;
+    int *offsets = malloc(sizeof(int *)*128);
+    int n;
     do {
-        input = malloc(256);
-        command = malloc(256);
-        printf("group-28@shell:~$ ");
-        gets(input);
-        command = read_user_input(input, command);
-        shell_status = launch(command);
-        free(command);
-        free(input);
+        memset(command, 0, sizeof(char *)*128);
+        memset(offsets, 0, sizeof(int *)*128);
+        cwd = getcwd(NULL, 0);
+        printf("\033[1m\033[33mgroup-28@shell\033[0m:\033[1m\033[35m%s\033[0m$ ", cwd);
+        fgets(input, sizeof(char)*256, stdin);
+        input[strlen(input)-1] = '\0';
+        int valid = read_user_input(input, command, &n, offsets);
+        if (!valid) {
+            continue;
+        }
+        shell_status = launch(command, n, offsets);
     } while (shell_status);
 }
 
